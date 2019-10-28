567a31aa84011c2d66dd8dcfbedb1a9c168f9b22
Andre Macena <andrmacena_@outlook.com>
2019-10-28 16:42:41 -0300

    Adicionado classes de configuração para criação da base de dados

---
+++ Data/Maps/CategoryMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data.Maps
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.HasKey(category => category.Id);
+            builder.Property(category => category.Title).IsRequired().HasMaxLength(120).HasColumnType("varchar(120)");
+        }
+    }
+}
---
+++ Data/Maps/ProductMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data.Maps
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+            builder.HasKey(prod => prod.Id);
+            builder.Property(prod => prod.CreateDate).IsRequired();
+            builder.Property(prod => prod.Description).IsRequired().HasMaxLength(1024).HasColumnType("varchar(1024)");
+            builder.Property(prod => prod.Image).IsRequired().HasMaxLength(1024).HasColumnType("varchar(1024)");
+            builder.Property(prod => prod.LastUpdateDate).IsRequired();
+            builder.Property(prod => prod.Price).IsRequired().HasColumnType("money");
+            builder.Property(prod => prod.Quantity).IsRequired();
+            builder.Property(prod => prod.Title).IsRequired().HasMaxLength(120).HasColumnType("varchar(120)");
+            builder.HasOne(prod => prod.Category).WithMany(products => products.Products);
+        }
+    }
+}
