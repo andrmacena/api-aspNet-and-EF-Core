b38b914517b8263b9303e4d705fc8bac43dd0758
Andre Macena <andrmacena_@outlook.com>
2019-10-28 16:17:16 -0300

    Adicionado classe StoreDataContext

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-I8RBCN3\SQLEXPRESS;Database=baltaProductCatalog;User ID=SA;Password=andremacena");
+        }
+    }
+}
