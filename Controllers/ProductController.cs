e27cbf7e3aa067152b35e8c11d4d073e575bd471
Andre Macena <andrmacena_@outlook.com>
2019-11-01 17:52:18 -0300

    Adicionado validações ao controller do produto

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Data;
+using ProductCatalog.ViewModels.ProductViewModels;
+using ProductCatalog.Models;
+using ProductCatalog.ViewModels;
+
+namespace ProductCatalog.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly StoreDataContext _context;
+
+        public ProductController(StoreDataContext context)
+        {
+            _context = context;
+        }
+
+        [Route("v1/products")]
+        [HttpGet]
+        public IEnumerable<ListProductViewModel> Get()
+        {
+            return _context.Products
+                .Include(x => x.Category)
+                .Select(x => new ListProductViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Price = x.Price,
+                    Category = x.Category.Title,
+                    CategoryID = x.CategoryId
+                }).AsNoTracking().ToList();
+        }
+
+        [Route("v1/products/{id}")]
+        [HttpGet]
+        public Product Get(int id)
+        {
+            return _context.Products.AsNoTracking().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        [Route("v1/products")]
+        [HttpPost]
+        public ResultViewModel Post([FromBody]EditorProductViewModel model)
+        {
+            model.Validate();
+            if (model.Invalid)
+                return new ResultViewModel
+                {
+                    Success = false,
+                    Message = "Não foi possível cadastrar o produto",
+                    Data = model.Notifications
+                };
+
+            var product = new Product();
+            product.Title = model.Title;
+            product.CategoryId = model.CategoryId;
+            product.CreateDate = DateTime.Now;
+            product.Description = model.Description;
+            product.Image = model.Image;
+            product.LastUpdateDate = DateTime.Now;
+            product.Price = model.Price;
+            product.Quantity = model.Quantity;
+
+            _context.Add(product);
+            _context.SaveChanges();
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto cadastro com sucesso",
+                Data = product
+            };
+        }
+
+        [Route("v1/products")]
+        [HttpPut]
+        public ResultViewModel Put([FromBody]EditorProductViewModel model)
+        {
+            model.Validate();
+            if (model.Invalid)
+                return new ResultViewModel
+                {
+                    Success = false,
+                    Message = "Não foi possível alterar o produto",
+                    Data = model.Notifications
+                };
+
+            var product = _context.Products.Find(model.Id);
+            product.Title = model.Title;
+            product.CategoryId = model.CategoryId;
+            //product.CreateDate = DateTime.Now;
+            product.Description = model.Description;
+            product.Image = model.Image;
+            product.LastUpdateDate = DateTime.Now;
+            product.Price = model.Price;
+            product.Quantity = model.Quantity;
+
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return new ResultViewModel
+            {
+                Success = true,
+                Message = "Produto editado com sucesso",
+                Data = product
+            };
+        }
+    }
+}
