fdddbc1d1cd3f2860fc6d4863f7bcb9f7cec45ca
Andre Macena <andrmacena_@outlook.com>
2019-11-01 17:51:11 -0300

    Adicionado ViewModels

---
+++ ViewModels/ProductViewModels/ListProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ProductCatalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.ViewModels.ProductViewModels
+{
+    public class ListProductViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryID { get; set; }
+        public string Category { get; set; }
+    }
+}
