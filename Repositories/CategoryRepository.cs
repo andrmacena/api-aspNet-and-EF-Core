23346204d38882934d596f2679f7095ab355f6e9
Andre Macena <andrmacena_@outlook.com>
2019-11-04 14:12:10 -0300

    Criado a classe CategoryRepository

---
+++ Repositories/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data;
+using ProductCatalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly StoreDataContext _context;
+
+        public CategoryRepository(StoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> Get()
+        {
+            return _context.Categories.AsNoTracking().ToList();
+        }
+
+        public Category Get(int id)
+        {
+            return _context.Categories.AsNoTracking().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public IEnumerable<Product> GetProductByID(int id)
+        {
+            return _context.Products.AsNoTracking().Where(x => x.CategoryId == id).ToList();
+        }
+
+        public void Save(Category category)
+        {
+            _context.Add(category);
+            _context.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Entry(category).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(Category category)
+        {
+            _context.Remove(category);
+            _context.SaveChanges();
+        }
+    }
+}
