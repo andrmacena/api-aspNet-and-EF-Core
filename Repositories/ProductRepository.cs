e3bb98fd9500eef5604ebb52081cb39f09cfbc6d
Andre Macena <andrmacena_@outlook.com>
2019-11-04 13:50:30 -0300

    Criado classe ProductRepository

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data;
+using ProductCatalog.Models;
+using ProductCatalog.ViewModels.ProductViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductCatalog.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly StoreDataContext _context;
+
+        public ProductRepository(StoreDataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ListProductViewModel> Get()
+        {
+            return _context.Products
+                .Include(x => x.Category)
+                .Select(x => new ListProductViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Price = x.Price,
+                    Category = x.Category.Title,
+                    CategoryID = x.CategoryId
+                }).AsNoTracking().ToList();
+        }
+
+        public Product Get(int id)
+        {
+            return _context.Products.Find(id);
+        }
+
+        public void Save(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
